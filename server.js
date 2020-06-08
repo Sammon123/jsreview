8497ec207a0208f860224e3c776d015ecb37251a
Peter Sammon <petersammon2@gmail.com>
2020-06-08 19:30:52 -0400

    added api and html routes with basic server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// added some comment
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`server is listening at http://localhost${PORT}`);
+
+})
